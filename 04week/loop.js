bb0f1e13c453aa0ccefb6e257ed76c081b2df957
jlmeister <iamphil4483@yahoo.com>
2019-09-10 23:23:50 -0500

    in class loops activity. first commit.

---
+++ 04week/loop.js
@@ -0,0 +1,31 @@
+"use strict"
+
+// 1. Use a do..while loop to console log the numbers from 1 to 1000.
+// let i = 0;
+
+// do {
+//   i++;
+//   console.log(i);
+// } while(i<1000);
+
+// 2. Create an object called person with the following data:
+// firstName: "Jane"
+// lastName: "Doe"
+// birthDate: "Jan 5, 1925"
+// gender: "female"
+
+const person = {
+  firstName: "Jane",
+  lastName: "Doe",
+  birthDate: "Jan 5, 1925",
+  gender: "female"
+}
+// console.log(person);
+
+// 3. Use a for...in loop and if statement to console.log the value associated with the key birthDate if the birth year is an odd number.
+
+for (let property in person) {
+  if (property == "birthDate") {
+    
+  }
+}
